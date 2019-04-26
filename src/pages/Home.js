ca4101dca13e78071e3ddb92a1bd45d2324ecb30
Bao Vu Hoang <baovuhbv@gmail.com>
2019-04-26 08:48:21 +0700

    Add Component CountCompleteTask from Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,24 @@
 import React, { useState, Fragment } from 'react';
-import { Row, Col, Card, Button } from 'reactstrap';
+import { Row, Col, Button } from 'reactstrap';
 import { Link } from "react-router-dom";
 import { Trans, withTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import * as config from '../constants/Config';
+import CountCompleteTask from '../components/dashboard/CountCompleteTask';
 
-function Home(props) {
+function Home() {
   let [items] = useState([]);
-  let { t } = props;
   items = JSON.parse(localStorage.getItem(config.ITEMS_FROM_LOCAL_STORAGE)) || [];
-
-  const countCompleteTask = boolean => {
-    return items.filter(item => item.complete === boolean).length;
-  }
-
-  let renderCompleteTask = t('TASK_NO_LIST');
-  if (items.length > 0) {
-    renderCompleteTask = <Trans i18nKey="TASK_UNDONE" count={countCompleteTask(false)} />;
-  }
 
   return (
     <Fragment>
       <Row>
         <Col md={4}>
-          <Card body inverse color="danger">
-            {renderCompleteTask}
-          </Card>
+          <CountCompleteTask items={items} color="danger" count={false} />
         </Col>
         <Col md={4}>
-          <Card body inverse color="success">
-            <Trans i18nKey="TASK_DONE" count={countCompleteTask(true)} />
-          </Card>
+          <CountCompleteTask items={items} color="success" count={true} />
         </Col>
         <Col md={4}>
           <Button color="primary" tag={Link} to="/todos/">
